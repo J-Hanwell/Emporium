6a6b4096a1402416eeedd26a5c9f5f661070d7ac
Joe Rawnsley <joe.rawnsley@gmail.com>
2023-03-18 16:25:17 +0000

    two-way Roman numeral converter is fully functional. Just need a few tweaks to layout

--- roman/fromRoman.js
+++ roman/fromRoman.js
@@ -1,19 +1,6 @@
+const { values } = require("./values")
+
 function fromRoman(string) {
-    const values = {
-        "M": 1000,
-        "CM": 900,
-        "D": 500,
-        "CD": 400,
-        "C": 100,
-        "XC": 90,
-        "L": 50,
-        "XL": 40,
-        "X": 10,
-        "IX": 9,
-        "V": 5,
-        "IV": 4,
-        "I": 1
-      }
     let input = string
     let accumulator = 0
     for (let i= 0; i < input.length; i ++) {
@@ -28,4 +15,6 @@ function fromRoman(string) {
     return accumulator
 }
 
-console.log(fromRoman("CDLVIII"))
+
+
+module.exports = { fromRoman }
